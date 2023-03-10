b1f4d6c09b68e669e0f12240210d7db0de109c1a
zmpxr <117150917+zmpxr@users.noreply.github.com>
2023-03-10 17:04:42 +0900

    Add files via upload

---
+++ sketch_7878.ino
@@ -0,0 +1,31 @@
+
+
+#include "Adafruit_CCS811.h"
+ 
+Adafruit_CCS811 ccs;
+ 
+void setup() {
+ Serial.begin(9600);
+ Serial.println("Amperka CCS811 test...");
+ if(!ccs.begin()){
+   Serial.println("Failed to start sensor! Please check your wiring.");
+   while(1);
+ }
+}
+ 
+void loop() {
+ if(ccs.available()) {
+   if(!ccs.readData()) {
+     // выводим в Serial-порт показания концентрации CO2
+     //количество летучих органических веществ
+     Serial.print("CO2: ");
+     Serial.print(ccs.geteCO2());
+     Serial.print("ppm, TVOC: ");
+     Serial.println(ccs.getTVOC());
+   } else {
+     Serial.println("ERROR...");
+     while(1);
+   }
+ }
+ delay(500);
+}
